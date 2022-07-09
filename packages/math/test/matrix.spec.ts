701f468e039f0dac648d2df8cb2bbfbb21d86403
Kim Jeong-won <threedalpeng@gmail.com>
2022-07-09 20:47:03 +0900

    test(math): update math tests

---
+++ packages/math/test/matrix.spec.ts
@@ -0,0 +1,17 @@
+import { describe, test } from "vitest";
+
+describe.todo("Vector", () => {
+  test.todo("add", () => {});
+  test.todo("sub", () => {});
+  test.todo("muli", () => {});
+  test.todo("divi", () => {});
+  test.todo("mulv", () => {});
+  test.todo("mul", () => {});
+  test.todo("div", () => {});
+  test.todo("normalize", () => {});
+  test.todo("size", () => {});
+  test.todo("size2", () => {});
+  test.todo("toAxisAngle", () => {});
+  test.todo("toMatrix", () => {});
+  test.todo("fromAxisAngle", () => {});
+});
--- packages/math/test/quaternion.spec.ts
+++ packages/math/test/quaternion.spec.ts
@@ -1,38 +1,71 @@
 import { describe, expect, test } from "vitest";
-import { Vec3 } from "../src";
-import { Quaternion } from "../src/Quaternion";
+import { Vec3, type AxisAngle, Quaternion } from "../src";
 
 const _1_0_0_90 = new Quaternion([0.7071068, 0, 0, 0.7071068]);
 
+const axisAngleDeepTest = (
+  [axis, angle]: AxisAngle,
+  [tAxis, tAngle]: AxisAngle
+) => {
+  function isInappropriateRatio(ratio) {
+    return (
+      isNaN(ratio) || !isFinite(ratio) || ratio === undefined || ratio === null
+    );
+  }
+  const ratio = isInappropriateRatio(axis.x / tAxis.x)
+    ? isInappropriateRatio(axis.y / tAxis.y)
+      ? isInappropriateRatio(axis.z / tAxis.z)
+        ? 0
+        : axis.z / tAxis.z
+      : axis.y / tAxis.y
+    : axis.x / tAxis.x;
+  expect(axis.x).toBeCloseTo(tAxis.x * ratio);
+  expect(axis.y).toBeCloseTo(tAxis.y * ratio);
+  expect(axis.z).toBeCloseTo(tAxis.z * ratio);
+  expect(((angle % 360) + 360) % 360).toBeCloseTo(((tAngle % 360) + 360) % 360);
+};
+
+const quaternionDeepTest = (q: Quaternion, t: Quaternion) => {
+  expect(q.x).toBeCloseTo(t.x);
+  expect(q.y).toBeCloseTo(t.y);
+  expect(q.z).toBeCloseTo(t.z);
+  expect(q.w).toBeCloseTo(t.w);
+};
+
 describe("quaternion", () => {
-  test("add", () => {});
-  test("sub", () => {});
-  test("muli", () => {});
-  test("divi", () => {});
-  test("mulv", () => {});
-  test("mul", () => {});
-  test("div", () => {});
-  test("normalize", () => {});
-  test("size", () => {});
-  test("size2", () => {});
+  test.todo("add", () => {});
+  test.todo("sub", () => {});
+  test.todo("muli", () => {});
+  test.todo("divi", () => {});
+  test.todo("mulv", () => {});
+  test.todo("mul", () => {});
+  test.todo("div", () => {});
+  test.todo("normalize", () => {});
+  test.todo("size", () => {});
+  test.todo("size2", () => {});
   test("toAxisAngle", () => {
-    expect(_1_0_0_90.toAxisAngle).to;
+    axisAngleDeepTest(_1_0_0_90.toAxisAngle(), [new Vec3([1, 0, 0]), 90]);
+    axisAngleDeepTest(
+      new Quaternion([0.3953694, 0.711665, 0, -0.580703]).toAxisAngle(),
+      [new Vec3([50, 90, 0]), 251]
+    );
+    axisAngleDeepTest(
+      new Quaternion([0.0391028, -0.11596, 0.2184363, 0.9681476]).toAxisAngle(),
+      [new Vec3([29, -86, 162]), -691]
+    );
   });
   test("toMatrix", () => {});
   test("fromAxisAngle", () => {
-    const deepTest = (q: Quaternion, t: Quaternion) => {
-      expect(q.x).toBeCloseTo(t.x);
-      expect(q.y).toBeCloseTo(t.y);
-      expect(q.z).toBeCloseTo(t.z);
-      expect(q.w).toBeCloseTo(t.w);
-    };
     const q1 = Quaternion.fromAxisAngle(new Vec3([1, 0, 0]), 90);
-    deepTest(q1, _1_0_0_90);
+    quaternionDeepTest(q1, _1_0_0_90);
 
     const q2 = Quaternion.fromAxisAngle(new Vec3([50, 90, 0]), 251);
-    deepTest(q2, new Quaternion([0.3953694, 0.711665, 0, -0.580703]));
+    quaternionDeepTest(q2, new Quaternion([0.3953694, 0.711665, 0, -0.580703]));
 
     const q3 = Quaternion.fromAxisAngle(new Vec3([29, -86, 162]), -691);
-    deepTest(q3, new Quaternion([0.0391028, -0.11596, 0.2184363, 0.9681476]));
+    quaternionDeepTest(
+      q3,
+      new Quaternion([0.0391028, -0.11596, 0.2184363, 0.9681476])
+    );
   });
 });
