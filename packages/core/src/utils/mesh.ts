5961711ae5ba7948a7aeba5e02ce9233c3a8b602
Kim Jeong-won <threedalpeng@gmail.com>
2022-08-03 23:54:49 +0900

    Create box mesh builder

--- packages/core/src/utils/mesh.ts
+++ packages/core/src/utils/mesh.ts
@@ -1,46 +1,54 @@
-export default class MeshBuilder2D {
-  static regularTraingle() {
-    return {
-      position: new Float32Array([
-        0,
-        -1 / Math.sqrt(3),
-        0.5,
-        (1 / Math.sqrt(3)) * 0.5,
-        -0.5,
-        (1 / Math.sqrt(3)) * 0.5,
-      ]),
-      color: new Uint8Array([255, 0, 0, 0, 255, 0, 0, 0, 255]),
-    };
-  }
+interface Mesh {
+  vertex: {
+    position: Float32Array;
+    normal: Float32Array;
+    texcoord: Float32Array;
+  };
+  index: Float32Array;
+}
 
-  static circle(division: number) {
-    const centerRadian = (2 * Math.PI) / division;
-    const mesh = {
-      position: new Float32Array(division * 3 * 2),
-      color: new Uint8Array(division * 3 * 3),
-    };
-    for (let i = 0; i < division; i++) {
-      const radian0 = centerRadian * i;
-      const radian1 = centerRadian * (i + 1);
-      const s0 = Math.sin(radian0);
-      const c0 = Math.cos(radian0);
-      const s1 = Math.sin(radian1);
-      const c1 = Math.cos(radian1);
-      mesh.position.set([c0, s0], i * 6);
-      mesh.position.set([c1, s1], i * 6 + 2);
-      mesh.position.set([0, 0], i * 6 + 4);
-      mesh.color.set([158, 123, 194, 158, 123, 194, 158, 123, 194], i * 9);
-    }
-    return mesh;
-  }
-
-  static rectangular() {
-    return {
-      position: new Float32Array([1, 1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1]),
-      color: new Uint8Array([
-        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-        255, 255, 255, 255,
+export default class MeshBuilder {
+  static sphere() {}
+  static box() {
+    const boxMesh: Mesh = {
+      // prettier-ignore
+      vertex: {
+        position: new Float32Array([
+          1, 1, 1,    1, 1, -1,     -1, 1, -1,  -1, 1, 1,   // top
+          -1, -1, 1,  -1, -1, -1,   1, -1, -1,  1, -1, 1,   // bottom
+          -1, 1, 1,   -1, 1, -1,    -1, -1, -1, -1, -1, 1,  // left
+          1, -1, 1,   1, -1, -1,    1, 1, -1,   1, 1, 1,    // right
+          1, 1, 1,    -1, 1, 1,     -1, -1, 1,  1, -1, 1,   // front
+          1, 1, -1,   1, -1, -1,    -1, -1, -1, -1, 1, -1,  // back
+        ]),
+        normal: new Float32Array([
+          0, 1, 0,    0, 1, 0,    0, 1, 0,    0, 1, 0,  // top
+          0, -1, 0,   0, -1, 0,   0, -1, 0,   0, -1, 0, // bottom
+          -1, 0, 0,   -1, 0, 0,   -1, 0, 0,   -1, 0, 0, // left
+          1, 0, 0,    1, 0, 0,    1, 0, 0,    1, 0, 0,  // right
+          0, 0, 1,    0, 0, 1,    0, 0, 1,    0, 0, 1,  // front
+          0, 0, -1,   0, 0, -1,   0, 0, -1,   0, 0, -1, // back
+        ]),
+        texcoord: new Float32Array([
+          0, 1,   1, 1,   1, 0,   0, 0, // top
+          0, 1,   1, 1,   0, 0,   0, 0, // bottom
+          0, 1,   1, 1,   1, 0,   0, 0, // left
+          0, 1,   1, 1,   1, 0,   0, 0, // right
+          0, 1,   1, 1,   1, 0,   0, 0, // front
+          0, 1,   1, 1,   1, 0,   0, 0, // back
+        ]),
+      },
+      // prettier-ignore
+      index: new Float32Array([
+        0, 1, 2,    0, 2, 3,    // top
+        4, 5, 6,    4, 6, 7,    // bottom
+        8, 9, 10,   8, 10, 11,  // left
+        12, 13, 14, 12, 14, 15, // right
+        16, 17, 18, 16, 18, 19, // front
+        20, 21, 22, 20, 22, 23, // back
       ]),
     };
   }
+  static cylinder() {}
+  static capsule() {}
 }
