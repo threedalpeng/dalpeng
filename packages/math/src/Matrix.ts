590fe26bb8e64b4512a30b5377cbe0343930379d
Kim Jeong-won <threedalpeng@gmail.com>
2022-08-03 23:07:06 +0900

    style(math): format matrices and disable auto-formatting

--- packages/math/src/Matrix.ts
+++ packages/math/src/Matrix.ts
@@ -59,42 +59,27 @@ export class Mat3 extends Float32Array {
   }
 
   add(m: Float32List) {
+    // prettier-ignore
     return new Mat3([
-      this[0] + m[0],
-      this[1] + m[1],
-      this[2] + m[2],
-      this[3] + m[3],
-      this[4] + m[4],
-      this[5] + m[5],
-      this[6] + m[6],
-      this[7] + m[7],
-      this[8] + m[8],
+      this[0] + m[0], this[1] + m[1], this[2] + m[2],
+      this[3] + m[3], this[4] + m[4], this[5] + m[5],
+      this[6] + m[6], this[7] + m[7], this[8] + m[8],
     ]);
   }
   sub(m: Float32List) {
+    // prettier-ignore
     return new Mat3([
-      this[0] - m[0],
-      this[1] - m[1],
-      this[2] - m[2],
-      this[3] - m[3],
-      this[4] - m[4],
-      this[5] - m[5],
-      this[6] - m[6],
-      this[7] - m[7],
-      this[8] - m[8],
+      this[0] - m[0], this[1] - m[1], this[2] - m[2],
+      this[3] - m[3], this[4] - m[4], this[5] - m[5],
+      this[6] - m[6], this[7] - m[7], this[8] - m[8],
     ]);
   }
   muli(i: number) {
+    // prettier-ignore
     return new Mat3([
-      this[0] * i,
-      this[1] * i,
-      this[2] * i,
-      this[3] * i,
-      this[4] * i,
-      this[5] * i,
-      this[6] * i,
-      this[7] * i,
-      this[8] * i,
+      this[0] * i, this[1] * i, this[2] * i,
+      this[3] * i, this[4] * i, this[5] * i,
+      this[6] * i, this[7] * i, this[8] * i,
     ]);
   }
   mulv(v: Float32List) {
@@ -105,16 +90,11 @@ export class Mat3 extends Float32Array {
     ]);
   }
   mul(m: Float32List) {
+    // prettier-ignore
     return new Mat3([
-      m[0] * this[0] + m[1] * this[3] + m[2] * this[6],
-      m[0] * this[1] + m[1] * this[4] + m[2] * this[7],
-      m[0] * this[2] + m[1] * this[5] + m[2] * this[8],
-      m[3] * this[0] + m[4] * this[3] + m[5] * this[6],
-      m[3] * this[1] + m[4] * this[4] + m[5] * this[7],
-      m[3] * this[2] + m[4] * this[5] + m[5] * this[8],
-      m[6] * this[0] + m[7] * this[3] + m[8] * this[6],
-      m[6] * this[1] + m[7] * this[4] + m[8] * this[7],
-      m[6] * this[2] + m[7] * this[5] + m[8] * this[8],
+      m[0] * this[0] + m[1] * this[3] + m[2] * this[6], m[0] * this[1] + m[1] * this[4] + m[2] * this[7], m[0] * this[2] + m[1] * this[5] + m[2] * this[8],
+      m[3] * this[0] + m[4] * this[3] + m[5] * this[6], m[3] * this[1] + m[4] * this[4] + m[5] * this[7], m[3] * this[2] + m[4] * this[5] + m[5] * this[8],
+      m[6] * this[0] + m[7] * this[3] + m[8] * this[6], m[6] * this[1] + m[7] * this[4] + m[8] * this[7], m[6] * this[2] + m[7] * this[5] + m[8] * this[8],
     ]);
   }
 
@@ -123,16 +103,11 @@ export class Mat3 extends Float32Array {
   }
 
   transpose() {
+    // prettier-ignore
     return new Mat3([
-      this[0],
-      this[3],
-      this[6],
-      this[1],
-      this[4],
-      this[7],
-      this[2],
-      this[5],
-      this[8],
+      this[0], this[3], this[6],
+      this[1], this[4], this[7],
+      this[2], this[5], this[8],
     ]);
   }
 
@@ -150,16 +125,12 @@ export class Mat3 extends Float32Array {
       console.error("inverse() might be singular.");
     }
     const s = 1 / d;
+
+    // prettier-ignore
     return new Mat3([
-      (this[4] * this[8] - this[7] * this[5]) * s,
-      (this[2] * this[7] - this[1] * this[8]) * s,
-      (this[1] * this[5] - this[2] * this[4]) * s,
-      (this[5] * this[6] - this[3] * this[8]) * s,
-      (this[0] * this[8] - this[2] * this[6]) * s,
-      (this[3] * this[2] - this[0] * this[5]) * s,
-      (this[3] * this[7] - this[6] * this[4]) * s,
-      (this[6] * this[1] - this[0] * this[7]) * s,
-      (this[0] * this[4] - this[3] * this[1]) * s,
+      (this[4] * this[8] - this[7] * this[5]) * s, (this[2] * this[7] - this[1] * this[8]) * s, (this[1] * this[5] - this[2] * this[4]) * s,
+      (this[5] * this[6] - this[3] * this[8]) * s, (this[0] * this[8] - this[2] * this[6]) * s, (this[3] * this[2] - this[0] * this[5]) * s,
+      (this[3] * this[7] - this[6] * this[4]) * s, (this[6] * this[1] - this[0] * this[7]) * s, (this[0] * this[4] - this[3] * this[1]) * s,
     ]);
   }
 
@@ -291,63 +262,30 @@ export class Mat4 extends Float32Array {
   }
 
   add(m: Float32List) {
+    // prettier-ignore
     return new Mat4([
-      this[0] + m[0],
-      this[1] + m[1],
-      this[2] + m[2],
-      this[3] + m[3],
-      this[4] + m[4],
-      this[5] + m[5],
-      this[6] + m[6],
-      this[7] + m[7],
-      this[8] + m[8],
-      this[9] + m[9],
-      this[10] + m[10],
-      this[11] + m[11],
-      this[12] + m[12],
-      this[13] + m[13],
-      this[14] + m[14],
-      this[15] + m[15],
+      this[0] + m[0],   this[1] + m[1],   this[2] + m[2],   this[3] + m[3],
+      this[4] + m[4],   this[5] + m[5],   this[6] + m[6],   this[7] + m[7],
+      this[8] + m[8],   this[9] + m[9],   this[10] + m[10], this[11] + m[11],
+      this[12] + m[12], this[13] + m[13], this[14] + m[14], this[15] + m[15],
     ]);
   }
   sub(m: Float32List) {
+    // prettier-ignore
     return new Mat4([
-      this[0] - m[0],
-      this[1] - m[1],
-      this[2] - m[2],
-      this[3] - m[3],
-      this[4] - m[4],
-      this[5] - m[5],
-      this[6] - m[6],
-      this[7] - m[7],
-      this[8] - m[8],
-      this[9] - m[9],
-      this[10] - m[10],
-      this[11] - m[11],
-      this[12] - m[12],
-      this[13] - m[13],
-      this[14] - m[14],
-      this[15] - m[15],
+      this[0] - m[0],   this[1] - m[1],   this[2] - m[2],   this[3] - m[3],
+      this[4] - m[4],   this[5] - m[5],   this[6] - m[6],   this[7] - m[7],
+      this[8] - m[8],   this[9] - m[9],   this[10] - m[10], this[11] - m[11],
+      this[12] - m[12], this[13] - m[13], this[14] - m[14], this[15] - m[15],
     ]);
   }
   muli(i: number) {
+    // prettier-ignore
     return new Mat4([
-      this[0] * i,
-      this[1] * i,
-      this[2] * i,
-      this[3] * i,
-      this[4] * i,
-      this[5] * i,
-      this[6] * i,
-      this[7] * i,
-      this[8] * i,
-      this[9] * i,
-      this[10] * i,
-      this[11] * i,
-      this[12] * i,
-      this[13] * i,
-      this[14] * i,
-      this[15] * i,
+      this[0] * i,  this[1] * i,  this[2] * i,  this[3] * i,
+      this[4] * i,  this[5] * i,  this[6] * i,  this[7] * i,
+      this[8] * i,  this[9] * i,  this[10] * i, this[11] * i,
+      this[12] * i, this[13] * i, this[14] * i, this[15] * i,
     ]);
   }
   mulv(v: Float32List) {
@@ -359,76 +297,43 @@ export class Mat4 extends Float32Array {
     ]);
   }
   mul(m: Float32List) {
+    // prettier-ignore
     return new Mat4([
-      m[0] * this[0] + m[1] * this[4] + m[2] * this[8] + m[3] * this[12],
-      m[0] * this[1] + m[1] * this[5] + m[2] * this[9] + m[3] * this[13],
-      m[0] * this[2] + m[1] * this[6] + m[2] * this[10] + m[3] * this[14],
-      m[0] * this[3] + m[1] * this[7] + m[2] * this[11] + m[3] * this[15],
-      m[4] * this[0] + m[5] * this[4] + m[6] * this[8] + m[7] * this[12],
-      m[4] * this[1] + m[5] * this[5] + m[6] * this[9] + m[7] * this[13],
-      m[4] * this[2] + m[5] * this[6] + m[6] * this[10] + m[7] * this[14],
-      m[4] * this[3] + m[5] * this[7] + m[6] * this[11] + m[7] * this[15],
-      m[8] * this[0] + m[9] * this[4] + m[10] * this[8] + m[11] * this[12],
-      m[8] * this[1] + m[9] * this[5] + m[10] * this[9] + m[11] * this[13],
-      m[8] * this[2] + m[9] * this[6] + m[10] * this[10] + m[11] * this[14],
-      m[8] * this[3] + m[9] * this[7] + m[10] * this[11] + m[11] * this[15],
-      m[12] * this[0] + m[13] * this[4] + m[14] * this[8] + m[15] * this[12],
-      m[12] * this[1] + m[13] * this[5] + m[14] * this[9] + m[15] * this[13],
-      m[12] * this[2] + m[13] * this[6] + m[14] * this[10] + m[15] * this[14],
-      m[12] * this[3] + m[13] * this[7] + m[14] * this[11] + m[15] * this[15],
+      m[0] * this[0] + m[1] * this[4] + m[2] * this[8] + m[3] * this[12],     m[0] * this[1] + m[1] * this[5] + m[2] * this[9] + m[3] * this[13],     m[0] * this[2] + m[1] * this[6] + m[2] * this[10] + m[3] * this[14],      m[0] * this[3] + m[1] * this[7] + m[2] * this[11] + m[3] * this[15],
+      m[4] * this[0] + m[5] * this[4] + m[6] * this[8] + m[7] * this[12],     m[4] * this[1] + m[5] * this[5] + m[6] * this[9] + m[7] * this[13],     m[4] * this[2] + m[5] * this[6] + m[6] * this[10] + m[7] * this[14],      m[4] * this[3] + m[5] * this[7] + m[6] * this[11] + m[7] * this[15],
+      m[8] * this[0] + m[9] * this[4] + m[10] * this[8] + m[11] * this[12],   m[8] * this[1] + m[9] * this[5] + m[10] * this[9] + m[11] * this[13],   m[8] * this[2] + m[9] * this[6] + m[10] * this[10] + m[11] * this[14],    m[8] * this[3] + m[9] * this[7] + m[10] * this[11] + m[11] * this[15],
+      m[12] * this[0] + m[13] * this[4] + m[14] * this[8] + m[15] * this[12], m[12] * this[1] + m[13] * this[5] + m[14] * this[9] + m[15] * this[13], m[12] * this[2] + m[13] * this[6] + m[14] * this[10] + m[15] * this[14],  m[12] * this[3] + m[13] * this[7] + m[14] * this[11] + m[15] * this[15],
     ]);
   }
 
   static identity() {
-    return new Mat4([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
+    // prettier-ignore
+    return new Mat4([
+      1, 0, 0, 0, 
+      0, 1, 0, 0, 
+      0, 0, 1, 0, 
+      0, 0, 0, 1
+    ]);
   }
 
   transpose() {
+    // prettier-ignore
     return new Mat4([
-      this[0],
-      this[4],
-      this[8],
-      this[12],
-      this[1],
-      this[5],
-      this[9],
-      this[13],
-      this[2],
-      this[6],
-      this[10],
-      this[14],
-      this[3],
-      this[7],
-      this[11],
-      this[15],
+      this[0], this[4], this[8],  this[12],
+      this[1], this[5], this[9],  this[13],
+      this[2], this[6], this[10], this[14],
+      this[3], this[7], this[11], this[15],
     ]);
   }
   det() {
+    // prettier-ignore
     return (
-      this[12] * this[9] * this[6] * this[3] -
-      this[8] * this[13] * this[6] * this[3] -
-      this[12] * this[5] * this[10] * this[3] +
-      this[4] * this[13] * this[10] * this[3] +
-      this[8] * this[5] * this[14] * this[3] -
-      this[4] * this[9] * this[14] * this[3] -
-      this[12] * this[9] * this[2] * this[7] +
-      this[8] * this[13] * this[2] * this[7] +
-      this[12] * this[1] * this[10] * this[7] -
-      this[0] * this[13] * this[10] * this[7] -
-      this[8] * this[1] * this[14] * this[7] +
-      this[0] * this[9] * this[14] * this[7] +
-      this[12] * this[5] * this[2] * this[11] -
-      this[4] * this[13] * this[2] * this[11] -
-      this[12] * this[1] * this[6] * this[11] +
-      this[0] * this[13] * this[6] * this[11] +
-      this[4] * this[1] * this[14] * this[11] -
-      this[0] * this[5] * this[14] * this[11] -
-      this[8] * this[5] * this[2] * this[15] +
-      this[4] * this[9] * this[2] * this[15] +
-      this[8] * this[1] * this[6] * this[15] -
-      this[0] * this[9] * this[6] * this[15] -
-      this[4] * this[1] * this[10] * this[15] +
-      this[0] * this[5] * this[10] * this[15]
+      this[12] * this[9] * this[6] * this[3]  - this[8] * this[13] * this[6] * this[3]  - this[12] * this[5] * this[10] * this[3] + this[4] * this[13] * this[10] * this[3] +
+      this[8] * this[5] * this[14] * this[3]  - this[4] * this[9] * this[14] * this[3]  - this[12] * this[9] * this[2] * this[7]  + this[8] * this[13] * this[2] * this[7] +
+      this[12] * this[1] * this[10] * this[7] - this[0] * this[13] * this[10] * this[7] - this[8] * this[1] * this[14] * this[7]  + this[0] * this[9] * this[14] * this[7] +
+      this[12] * this[5] * this[2] * this[11] - this[4] * this[13] * this[2] * this[11] - this[12] * this[1] * this[6] * this[11] + this[0] * this[13] * this[6] * this[11] +
+      this[4] * this[1] * this[14] * this[11] - this[0] * this[5] * this[14] * this[11] - this[8] * this[5] * this[2] * this[15]  + this[4] * this[9] * this[2] * this[15] +
+      this[8] * this[1] * this[6] * this[15]  - this[0] * this[9] * this[6] * this[15]  - this[4] * this[1] * this[10] * this[15] + this[0] * this[5] * this[10] * this[15]
     );
   }
 
@@ -555,13 +460,25 @@ export class Mat4 extends Float32Array {
   }
 
   static translate(v: Float32List) {
-    return new Mat4([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, v[0], v[1], v[2], 1]);
+    // prettier-ignore
+    return new Mat4([
+      1,    0,    0,    0, 
+      0,    1,    0,    0, 
+      0,    0,    1,    0, 
+      v[0], v[1], v[2], 1
+    ]);
   }
   static rotate(q: Quaternion) {
     return q.toMatrix();
   }
   static scale(v: Float32List) {
-    return new Mat4([v[0], 0, 0, 0, 0, v[1], 0, 0, 0, 0, 0, v[2], 0, 0, 0, 1]);
+    // prettier-ignore
+    return new Mat4([
+      v[0], 0,    0,    0, 
+      0,    v[1], 0,    0, 
+      0,    0,    v[2], 0, 
+      0,    0,    0,    1
+    ]);
   }
 
   static fromAxisAngle(axis: Float32List, angle: number) {
@@ -571,23 +488,13 @@ export class Mat4 extends Float32Array {
     const x = axis[0];
     const y = axis[1];
     const z = axis[2];
+
+    // prettier-ignore
     return new Mat4([
-      x * x * (1 - c) + c,
-      x * y * (1 - c) + z * s,
-      x * z * (1 - c) - y * s,
-      0,
-      x * y * (1 - c) - z * s,
-      y * y * (1 - c) + c,
-      y * z * (1 - c) + x * s,
-      0,
-      x * z * (1 - c) + y * s,
-      y * z * (1 - c) - x * s,
-      z * z * (1 - c) + c,
-      0,
-      0,
-      0,
-      0,
-      1,
+      x * x * (1 - c) + c,      x * y * (1 - c) + z * s,  x * z * (1 - c) - y * s,  0,
+      x * y * (1 - c) - z * s,  y * y * (1 - c) + c,      y * z * (1 - c) + x * s,  0,
+      x * z * (1 - c) + y * s,  y * z * (1 - c) - x * s,  z * z * (1 - c) + c,      0,
+      0,                        0,                        0,                        1,
     ]);
   }
 }
