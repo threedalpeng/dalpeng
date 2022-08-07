c285ad3f4a684011b45226163e2deab3330965e7
Kim Jeong-won <threedalpeng@gmail.com>
2022-08-07 20:31:06 +0900

    Update build settings
    - Migrate vite to 3 and fix build setting
    - Fix that path alias not resolved in the built d.ts files

--- scripts/build-watch.sh
+++ scripts/build-watch.sh
@@ -1,3 +1,3 @@
-pnpm -C ./packages/math build --watch &
-pnpm -C ./packages/core build --watch &
-pnpm -C ./packages/dalpeng build --watch
+pnpm -C ./packages/math build --watch & 
+sleep 5 && pnpm -C ./packages/core build --watch & 
+sleep 10 && pnpm -C ./packages/dalpeng build --watch
