933e1d5bf03874bd7f312ee33776f72c4e2bbfa5
Kim Jeong-won <threedalpeng@gmail.com>
2022-08-07 20:48:40 +0900

    Create sphere mesh builder

--- packages/core/src/utils/mesh.ts
+++ packages/core/src/utils/mesh.ts
@@ -50,8 +50,132 @@ export default class MeshBuilder {
     return boxMesh;
   }
 
-  static sphere() {}
+  static sphere(): Mesh {
+    const DIVISION = 60;
+    const theta = Math.PI / DIVISION;
+    const phi = 2 * theta;
 
-  static cylinder() {}
+    const spherePoints = [0, 1, 0];
+    const texcoord = [1, 1];
+    for (let i = 1; i <= DIVISION - 1; i++) {
+      const t = theta * i;
+      const st = Math.sin(t);
+      const ct = Math.cos(t);
+      const vtc = (DIVISION - i) / DIVISION;
+
+      for (let j = 0; j <= DIVISION; j++) {
+        const p = phi * j;
+        const sp = Math.sin(p);
+        const cp = Math.cos(p);
+        spherePoints.push(st * cp, ct, st * sp);
+        texcoord.push((DIVISION - j) / (DIVISION + 1), vtc);
+      }
+    }
+    spherePoints.push(0, -1, 0);
+    texcoord.push(0, 0);
+
+    const indices = [];
+    for (let i = 0; i < DIVISION; i++) {
+      indices.push(0, i + 2, i + 1);
+    }
+    for (let j = 1; j < DIVISION; j++) {
+      for (let i = 0; i < DIVISION; i++) {
+        indices.push(
+          (j - 1) * (DIVISION + 1) + i + 1,
+          (j - 1) * (DIVISION + 1) + i + 2,
+          j * (DIVISION + 1) + i + 1,
+          j * (DIVISION + 1) + i + 1,
+          (j - 1) * (DIVISION + 1) + i + 2,
+          j * (DIVISION + 1) + i + 2
+        );
+      }
+    }
+    for (let i = 0; i < DIVISION; i++) {
+      indices.push(
+        (DIVISION - 2) * (DIVISION + 1) + i + 1,
+        (DIVISION - 2) * (DIVISION + 1) + i + 2,
+        DIVISION * DIVISION
+      );
+    }
+
+    return {
+      vertex: {
+        position: new Float32Array(spherePoints),
+        normal: new Float32Array(spherePoints),
+        texcoord: new Float32Array(texcoord),
+      },
+      index: new Uint16Array(indices),
+    };
+  }
+
+  static cylinder() {
+    const DIVISION = 60;
+    const theta = (2 * Math.PI) / DIVISION;
+
+    // top
+    const position = [0, 1, 0];
+    const normal = [0, 1, 0];
+    const texcoord = [0, 0];
+    for (let i = 0; i <= DIVISION; i++) {
+      const t = theta * i;
+      const st = Math.sin(t);
+      const ct = Math.cos(t);
+      position.push(st, 1, ct);
+      normal.push(0, 1, 0);
+      texcoord.push(0, 0);
+    }
+    // bottom
+    position.push(0, -1, 0);
+    normal.push(0, -1, 0);
+    texcoord.push(0, 0);
+    for (let i = 0; i <= DIVISION; i++) {
+      const t = theta * i;
+      const st = Math.sin(t);
+      const ct = Math.cos(t);
+      position.push(st, -1, ct);
+      normal.push(0, -1, 0);
+      texcoord.push(0, 0);
+    }
+    //side
+    for (let i = 0; i <= DIVISION; i++) {
+      const t = theta * i;
+      const st = Math.sin(t);
+      const ct = Math.cos(t);
+      position.push(st, 1, ct);
+      position.push(st, -1, ct);
+      normal.push(st, 0, ct);
+      normal.push(st, 0, ct);
+      texcoord.push(0, 0);
+      texcoord.push(0, 0);
+    }
+
+    const indices = [];
+    const bottomOffset = DIVISION + 2;
+    const sideOffset = bottomOffset * 2;
+    for (let i = 0; i < DIVISION; i++) {
+      const sidePrimitiveOffset = sideOffset + i * 2;
+      indices.push(0, i + 1, i + 2);
+      indices.push(bottomOffset, bottomOffset + i + 2, bottomOffset + i + 1);
+      indices.push(
+        sidePrimitiveOffset,
+        sidePrimitiveOffset + 1,
+        sidePrimitiveOffset + 2
+      );
+      indices.push(
+        sidePrimitiveOffset + 1,
+        sidePrimitiveOffset + 3,
+        sidePrimitiveOffset + 2
+      );
+    }
+
+    return {
+      vertex: {
+        position: new Float32Array(position),
+        normal: new Float32Array(normal),
+        texcoord: new Float32Array(texcoord),
+      },
+      index: new Uint16Array(indices),
+    };
+  }
   static capsule() {}
 }
