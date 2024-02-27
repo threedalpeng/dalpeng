c05c5c2accde1edcdef1eabde23495e33cb9b330
Kim Jeong-won <threedalpeng@gmail.com>
2024-02-28 05:52:30 +0900

    feat(demo/basic-3d-objects): add app and scene names

--- demo/basic-3d-objects/src/app/scene/Scene.ts
+++ demo/basic-3d-objects/src/app/scene/Scene.ts
@@ -1,10 +1,11 @@
-import { defineScene } from "dalpeng";
+import { defineScene, withName } from "dalpeng";
 import Camera from "./entities/Camera";
-import Ground from "./entities/Ground";
 import Group from "./entities/Group";
 import Light1 from "./entities/Light1";
 import Light2 from "./entities/Light2";
 
 export default defineScene(() => {
+  withName("Basic 3D Objects Scene");
+
   return [Group, Light1, Camera, Light2];
 });
