5c1f86c7dcb7233fa1e02686637cc5734fb7301a
Kim Jeong-won <threedalpeng@gmail.com>
2024-02-28 05:52:30 +0900

    Update demo

--- demo/basic-3d-objects/src/app/App.ts
+++ demo/basic-3d-objects/src/app/App.ts
@@ -1,6 +1,7 @@
-import { defineApp } from "dalpeng";
+import { defineApp, withName } from "dalpeng";
 import Scene from "./scene/Scene";
 
 export default defineApp(() => {
+  withName("Basic 3D Objects Application");
   return Scene;
 });
